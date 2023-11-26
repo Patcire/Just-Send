865673017f04c5cc86cedb4c0e8be8e6f4193f4c
Patricio <pcifrey501@g.educaand.es>
2023-11-26 19:28:59 +0100

    refactorización app.js: mejor organización del código

---
+++ README.md
@@ -0,0 +1,26 @@
+# Just Send
+
+###### DWEC T4 - P3 AMPLIACIÓN DE FUNCIONALIDADES
+
+##### *Por Patricio Cifredo*
+
+![miweb](/recursos/Captura.JPG)
+
+******
+
+
+0. App desplegada -->
+=
+
+0. Probar en local:
+
+1. Just Send - Introducción
+
+En este proyecto debíamos ampliar las funcionalidades de 
+uno de los proyectos con los que habíamos trabajado en la unidad.
+En mi caso, he escogido el proyecto mail, ya que era
+el que más me llamaba la atención, pues las funcionalidades
+que se le podían añadir eran bastante distintas al resto
+de proyectos.
+
+2. 
--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-
+// Just Send
 
 // Objetos - Variables
 
 // Con autoincremento generaremos el id para cada correo
 
 let  autoincremento = -1
-
 
 const objeto_email = {
     id_correo:autoincremento,
@@ -15,7 +14,6 @@ const objeto_email = {
     fecha: 'Por determinar',
     adjuntos: []
 }
-
 
 // Selectores
 
@@ -30,8 +28,9 @@ const spinner = document.querySelector('#spinner')
 const zona_arrastre = document.querySelector('#drop_zone')
 const frase_arrastre = document.querySelector('.frase_arrastre')
 
-
 // Funciones
+
+// Nuevas Funcionalidades implementadas
 
 const recuperar_id_historial = () => {
 
@@ -45,88 +44,8 @@ const recuperar_id_historial = () => {
 
 }
 
-const activar_spinner=(e)=>{
-    e.preventDefault()
-    spinner.classList.remove('hidden')
-    setTimeout(()=>{
-        spinner.classList.add('hidden')
-        resetear_formulario()
-
-        // creamos un mensaje de toodo se ha enviado ok
-        const mensaje_ok = document.createElement('p')
-        mensaje_ok.classList.add('ok','bg-green-500', 'text-white', 'text-center', 'rounded-lg', 'mt-10', 'text-sm')
-        mensaje_ok.textContent='Enviado con exito'
-        formulario.appendChild(mensaje_ok)
-
-        setTimeout(()=>{
-            mensaje_ok.remove()
-        }, 1500)
-
-    }, 1500)
-
-}
-
-const resetear_formulario=()=>{
-    objeto_email.email=''
-    objeto_email.asunto=''
-    objeto_email.mensaje=''
-    formulario.reset()
-}
-
-
 const almacenar_campos = () =>{
-    const campos_a_almacenar = JSON.stringify(info_campos)
-}
-
-
-const validar = (e) =>{
-    if (e.target.value.trim()===''){
-        mostrar_error(`Campo ${e.target.id} es obligatorio`, e.target.parentElement)
-        objeto_email[e.target.id] = ''
-        return
-    }
-  if (e.target.id==='email' && !validar_email(e.target.value)) {
-      mostrar_error('Email no válido', e.target.parentElement)
-      objeto_email[e.target.id] = ''
-      return
-  }
-
-  limpiar_alerta(e.target.parentElement)
-    objeto_email[e.target.id] = e.target.value.trim().toLowerCase()
-    comprobar_objeto(objeto_email)
-}
-
-const comprobar_objeto=() =>{
-     const values = Object.values(objeto_email)
-    if (values.includes('')){
-        boton_submit.classList.add('opacity-50')
-        boton_submit.disabled= true
-        return
-    }
-    boton_submit.classList.remove('opacity-50')
-    boton_submit.disabled= false
-
-}
-
-const limpiar_alerta = (referencia) => {
-    const alerta = referencia.querySelector('.alerta')
-    if (alerta) {
-        alerta.remove()
-    }
-}
-const mostrar_error=(error, referencia)=>{
-
-    limpiar_alerta(referencia)
-    const parrafo_error = document.createElement('p')
-    parrafo_error.textContent = error
-    //le añadimos estas clases ya creadas en css menos alerta que la añado yo
-    parrafo_error.classList.add('alerta','bg-red-600', 'text-center', 'text-white', 'p-2')
-    referencia.appendChild(parrafo_error)
-}
-
-const validar_email = (email) =>{
-    const regex_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-    return email.match(regex_email)
+     JSON.stringify(info_campos)
 }
 
 const recuperar_localStorage = (clave) =>{
@@ -193,27 +112,6 @@ const mostrar_historial = ()  => {
     }
 }
 
-// Página hace referencia a si estamos en historial / borradores
-const eliminar_correo = (id_correo, pagina) => {
-    console.log('entra en eliminar')
-    const lista_correos = JSON.parse(localStorage.getItem(pagina))
-    console.log(lista_correos)
-    const lista_correo_actualizada = lista_correos.filter(function(correo) {
-        console.log(typeof correo.id_correo)
-        console.log(typeof id_correo)
-        return correo.id_correo !== parseInt(id_correo)
-    } )
-    console.log(lista_correo_actualizada)
-    localStorage.setItem(pagina, JSON.stringify(lista_correo_actualizada))
-}
-
-const limpiar_html=(elemento)=>{
-    while (elemento.firstElementChild){
-        elemento.firstElementChild.remove()
-    }
-}
-
-
 const lanzar_modal_email = (correo) => {
     console.log('entra en lanzar_modal')
     const modal = document.createElement('section');
@@ -266,8 +164,21 @@ const lanzar_modal_email = (correo) => {
     });
 }
 
+// Página hace referencia a si estamos en index/historial
+const eliminar_correo = (id_correo, pagina) => {
+    console.log('entra en eliminar')
+    const lista_correos = JSON.parse(localStorage.getItem(pagina))
+    console.log(lista_correos)
+    const lista_correo_actualizada = lista_correos.filter(function(correo) {
+        console.log(typeof correo.id_correo)
+        console.log(typeof id_correo)
+        return correo.id_correo !== parseInt(id_correo)
+    } )
+    console.log(lista_correo_actualizada)
+    localStorage.setItem(pagina, JSON.stringify(lista_correo_actualizada))
+}
+
 const soltar = (e) => {
-    console.log("Fichero(s) arrastrados");
     e.preventDefault();
     if (e.dataTransfer.items) {
         for (let i = 0 ; i < e.dataTransfer.items.length; i++) {
@@ -287,7 +198,6 @@ const soltar = (e) => {
     }
     eliminar_arrastrado(e)
 }
-
 
 const  prevenir_default_arrastrar = (e) => {
     // por defecto las páginas de los navegadores
@@ -321,7 +231,86 @@ const enviar_email_emailjs = ( ) => {
     emailjs.send("service_6v2caxk","template_4g7o4xh", params)
 }
 
+// Funcionalidades antiguas (de la 1º entrega)
 
+const activar_spinner=(e)=>{
+    e.preventDefault()
+    spinner.classList.remove('hidden')
+    setTimeout(()=>{
+        spinner.classList.add('hidden')
+        resetear_formulario()
+
+        // creamos un mensaje de toodo se ha enviado ok
+        const mensaje_ok = document.createElement('p')
+        mensaje_ok.classList.add('ok','bg-green-500', 'text-white', 'text-center', 'rounded-lg', 'mt-10', 'text-sm')
+        mensaje_ok.textContent='Enviado con exito'
+        formulario.appendChild(mensaje_ok)
+
+        setTimeout(()=>{
+            mensaje_ok.remove()
+        }, 1500)
+
+    }, 1500)
+
+}
+
+const resetear_formulario=()=>{
+    objeto_email.email=''
+    objeto_email.asunto=''
+    objeto_email.mensaje=''
+    formulario.reset()
+}
+const comprobar_objeto=() =>{
+    const values = Object.values(objeto_email)
+    if (values.includes('')){
+        boton_submit.classList.add('opacity-50')
+        boton_submit.disabled= true
+        return
+    }
+    boton_submit.classList.remove('opacity-50')
+    boton_submit.disabled= false
+
+}
+const limpiar_alerta = (referencia) => {
+    const alerta = referencia.querySelector('.alerta')
+    if (alerta) {
+        alerta.remove()
+    }
+}
+const mostrar_error=(error, referencia)=>{
+
+    limpiar_alerta(referencia)
+    const parrafo_error = document.createElement('p')
+    parrafo_error.textContent = error
+    //le añadimos estas clases ya creadas en css menos alerta que la añado yo
+    parrafo_error.classList.add('alerta','bg-red-600', 'text-center', 'text-white', 'p-2')
+    referencia.appendChild(parrafo_error)
+}
+const validar_email = (email) =>{
+    const regex_email = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return email.match(regex_email)
+}
+const limpiar_html=(elemento)=>{
+    while (elemento.firstElementChild){
+        elemento.firstElementChild.remove()
+    }
+}
+const validar = (e) =>{
+    if (e.target.value.trim()===''){
+        mostrar_error(`Campo ${e.target.id} es obligatorio`, e.target.parentElement)
+        objeto_email[e.target.id] = ''
+        return
+    }
+    if (e.target.id==='email' && !validar_email(e.target.value)) {
+        mostrar_error('Email no válido', e.target.parentElement)
+        objeto_email[e.target.id] = ''
+        return
+    }
+
+    limpiar_alerta(e.target.parentElement)
+    objeto_email[e.target.id] = e.target.value.trim().toLowerCase()
+    comprobar_objeto(objeto_email)
+}
 
 // Eventos - Listeners
 
